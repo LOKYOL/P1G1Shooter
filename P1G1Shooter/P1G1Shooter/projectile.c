ad864c080357e59b6b3639044b2b7a2e1730bdfd
T14OnGithub <personnenejeej@gmail.com>
2021-10-29 14:53:48 +0200

    mouvement de projectile

--- P1G1Shooter/P1G1Shooter/projectile.c
+++ P1G1Shooter/P1G1Shooter/projectile.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include "projectile.h"
 
-void InitProj(projectile** proj, int speed, int direction, int PosX, int PosY, int SizeX, int SizeY, char Color) {
+void InitProj(projectile** proj, int speed, int direction, int PosX, int PosY, int SizeX, int SizeY, char Color)
+{
 	(*proj) = (projectile*)malloc(sizeof(projectile));
-	(*proj)->speed = speed;
+	(*proj)->projEntity->vitesse = speed;
 	(*proj)->direction = direction;
 	(*proj)->PosX = PosX;
-	(*proj)->PosY = PosY;
 	(*proj)->SizeX = SizeX;
 	(*proj)->SizeY = SizeY;
 	(*proj)->Color = Color;
+}
+
+void RefreshProjMovement(projectile** proj, int deltatime)
+{
+	if ((*proj)->direction)
+	{
+		(*proj)->PosX += (*proj)->projEntity->vitesse * deltatime;
+	}
+	else
+	{
+		(*proj)->PosX += -(*proj)->projEntity->vitesse * deltatime;
+	}
 }
