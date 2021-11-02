8a14ceed956feda8e55b0b1dff178362eb4f9f16
Tristan Léger <49200515+LegerTristan@users.noreply.github.com>
2021-11-02 13:26:56 +0100

    Change projectile Entity* to Entity

--- P1G1Shooter/P1G1Shooter/projectile.c
+++ P1G1Shooter/P1G1Shooter/projectile.c
@@ -5,12 +5,11 @@
 void InitProj(Projectile** proj, int speed, int direction/*, int PosX, int PosY, int SizeX, int SizeY*/)
 {
 	(*proj) = (Projectile*)malloc(sizeof(Projectile));
-	(*proj)->projEntity = (Entity*)malloc(sizeof(Entity));
-	InitDisplayZone(&(*proj)->projEntity->displayZone, 0, 0, 2, 1, 1);
-	(*proj)->projEntity->health = 1;
-	(*proj)->projEntity->damages = 1;
-	(*proj)->projEntity->speed = speed;
-	(*proj)->projEntity->update = UpdateProjMovement;
+	InitDisplayZone(&(*proj)->projEntity.displayZone, 0, 0, 2, 1, 1);
+	(*proj)->projEntity.health = 1;
+	(*proj)->projEntity.damages = 1;
+	(*proj)->projEntity.speed = speed;
+	(*proj)->projEntity.update = UpdateProjMovement;
 	(*proj)->direction = 1;
 	/*(*proj)->position.x = PosX;
 	(*proj)->position.y = PosY;
@@ -22,17 +21,16 @@ void UpdateProjMovement(Projectile** proj, int deltatime)
 {
 	if ((*proj)->direction)
 	{
-		(*proj)->projEntity->displayZone.mPosX += (*proj)->projEntity->speed * deltatime;
+		(*proj)->projEntity.displayZone.mPosX += (*proj)->projEntity.speed * deltatime;
 	}
 	else
 	{
-		(*proj)->projEntity->displayZone.mPosX -= (*proj)->projEntity->speed * deltatime;
+		(*proj)->projEntity.displayZone.mPosX -= (*proj)->projEntity.speed * deltatime;
 	}
 }
 
 void DestroyProjectile(Projectile* projectile)
 {
-	free(projectile->projEntity);
 	free(projectile);
 }
 
