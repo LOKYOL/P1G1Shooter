031392bd05aba75a248857c51e398a9293a406bc
Mathis <mathischaussemy@gmail.com>
2021-11-02 16:54:53 +0100

    Obstacle fonctionnel
    sauf les collisions

--- P1G1Shooter/P1G1Shooter/Engine/Game.h
+++ P1G1Shooter/P1G1Shooter/Engine/Game.h
@@ -4,11 +4,14 @@
 #include "GameState.h"
 #include "../Inputs.h"
 #include "../Entity.h"
+#include "../Obstacle.h"
 
 #define WINDOW_HEIGHT		50
 #define WINDOW_WIDTH		100
 #define WINDOW_FONT_SIZE	13
 #define WINDOW_FONT_SQUARED	1
+
+#define OBSTACLE_SPAWN_TIMER	5
 
 typedef struct Game
 {
@@ -22,6 +25,7 @@ typedef struct Game
 
 	double				mGameTime;
 	double				mGameDt;
+	double				mGameSpawnObstacleTimer;
 }Game;
 
 void	InitGame(Game* game);
@@ -31,6 +35,8 @@ int		MainLoop(Game* game);
 void	PushGameState(Game* game, GameState state);
 void	PopGameState(Game* game);
 void	ChangeGameState(Game* game, GameState state);
+
+void	SpawnObstacle(Game* game);
 
 void	PushEntity(Game* game, Entity** entity);
 void	PopEntity(Game* game, Entity* entity);
