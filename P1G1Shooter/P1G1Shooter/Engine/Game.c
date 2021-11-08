1a730c3b9efd7668f017dcfc33fbfb0341359f3b
PyerreV <93371728+PyerreV@users.noreply.github.com>
2021-11-08 14:41:43 +0100

    Update Game.c

--- P1G1Shooter/P1G1Shooter/Engine/Game.c
+++ P1G1Shooter/P1G1Shooter/Engine/Game.c
@@ -11,7 +11,7 @@ void	InitGame(Game* game)
 {
 	game->mScore = 0;
 	InitDisplayZone(&game->mScoreDisplayZone, 0, 0, 10, 10, 0);
-	PrintInDisplayZone(&game->mScoreDisplayZone, WHITE, BLACK, 0, 0, "Score: ", 0, NO_FLAG);
+	PrintInDisplayZone(&game->mScoreDisplayZone, WHITE, BLACK, 0, 0, "Score: 0", 0, NO_FLAG);
 
 	game->mDisplaySettings = InitDisplay
 	(
