8c97689877aaae3e383c127ccd45651c197130bd
Rygasary <rygasary@gmail.com>
2021-11-09 11:40:56 +0100

    Plus de commentaires!
    Oui

--- P1G1Shooter/P1G1Shooter/PlayerStruct.h
+++ P1G1Shooter/P1G1Shooter/PlayerStruct.h
@@ -24,20 +24,63 @@ typedef struct Player
 	float mShootCooldown;
 } Player;
 
+/// <summary>
+/// Initialize a player
+/// </summary>
+/// <param name="player">Player to initialize</param>
+/// <param name="gameScreen">Gamescreen to display player on</param>
 void InitPlayer(Player** player, GameScreenData* gameScreen);
 
+/// <summary>
+/// Update the player's
+/// </summary>
+/// <param name="player">Player the update</param>
+/// <param name="game">Current game</param>
+/// <param name="gameScreen">Datas bind to the game state</param>
 void Player_Update(void* player, struct Game* game, GameScreenData* gameScreen);
 
+/// <summary>
+/// Update the player's movements
+/// </summary>
+/// <param name="player">Player the update</param>
+/// <param name="game">Current game</param>
 void Player_UpdateMovement(Player* player, struct Game* game);
 
+/// <summary>
+/// Prevents a player from going out of bound 
+/// </summary>
+/// <param name="player">Player to check</param>
+/// <param name="posX">X position to check</param>
+/// <param name="posY">Y position to check</param>
 void ClampPlayerPos(Player* player, double* posX, double* posY);
 
+/// <summary>
+/// Makes the player shoot
+/// </summary>
+/// <param name="player">Player the update</param>
+/// <param name="_gameScreen">Datas bind to the game state</param>
 void Player_Shoot(Player* player, GameScreenData* _gameScreen);
 
+/// <summary>
+/// Updates the overshoot display of the player
+/// </summary>
+/// <param name="player">Player to check</param>
 void UpdateBatteryDisplayZonePosition(Player* player);
 
+/// <summary>
+/// Updates the health bar display of the player
+/// </summary>
+/// <param name="player">Player to check</param>
 void UpdateHealthDisplayZonePosition(Player* player);
 
+/// <summary>
+/// Creates the overshoot display of the player
+/// </summary>
+/// <param name="player">Player to check</param>
 void DrawBatteryInDisplayZone(Player* player);
 
+/// <summary>
+/// Creates the health bar display of the player
+/// </summary>
+/// <param name="player">Player to check</param>
 void DrawHealthInDisplayZone(Player* player);
