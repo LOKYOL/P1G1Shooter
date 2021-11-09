98311fcdf3476071a45468d958cb56303f321723
Rygasary <rygasary@gmail.com>
2021-11-09 11:01:08 +0100

    Commentaires game.h
    Plus de commentaires!

--- P1G1Shooter/P1G1Shooter/Engine/Game.h
+++ P1G1Shooter/P1G1Shooter/Engine/Game.h
@@ -28,11 +28,42 @@ typedef struct Game
 	DisplayZone			mScoreDisplayZone;
 }Game;
 
+/// <summary>
+/// Initialize the game
+/// </summary>
+/// <param name="game">Game to initialize</param>
 void	InitGame(Game* game);
+
+/// <summary>
+/// Closes the game and free all alocated memory
+/// </summary>
+/// <param name="game">Game to close</param>
 void	CloseGame(Game* game);
+
+/// <summary>
+/// Continues the main game loop
+/// </summary>
+/// <param name="game">Game to run</param>
+/// <returns>0 to keep going, anything else if finished/error</returns>
 int		MainLoop(Game* game);
 
+/// <summary>
+/// Pushes a gamestate
+/// </summary>
+/// <param name="game">Game to update</param>
+/// <param name="state">Gamestate to push</param>
 void	PushGameState(Game* game, GameState state);
+
+/// <summary>
+/// Pops a gamestate
+/// </summary>
+/// <param name="game">Game to get gamestate from</param>
 void	PopGameState(Game* game);
+
+/// <summary>
+/// Swiches the current gamestate with another
+/// </summary>
+/// <param name="game">Game to switch gamestate from</param>
+/// <param name="state">New gamestate</param>
 void	ChangeGameState(Game* game, GameState state);
 
