67f79c3a2c89437684a038b305220a3193f011e7
Rygasary <rygasary@gmail.com>
2021-11-09 09:58:34 +0100

    Commentaires entity.h et obstacle.h
    Commentaires

--- P1G1Shooter/P1G1Shooter/Obstacle.h
+++ P1G1Shooter/P1G1Shooter/Obstacle.h
@@ -8,8 +8,24 @@ typedef struct Obstacle
 	Entity mEntity;
 } Obstacle;
 
+/// <summary>
+/// Initialize an obstacle
+/// </summary>
+/// <param name="obstacle">Obstacle to initialize</param>
 void Obstacle_Initialize(Obstacle** obstacle);
 
+/// <summary>
+/// Update an obstacle's movement and appearance
+/// </summary>
+/// <param name="obstacle">Obstacle to update</param>
+/// <param name="game">Game of obstacle</param>
+/// <param name="gameScreen">Screen to update</param>
 void Obstacle_Update(Obstacle* obstacle, Game* game, GameScreenData* gameScreen);
 
-void Obstacle_UpdateMovement(Obstacle* obstacle, Game* game, GameScreenData* _gameScreen);
+/// <summary>
+/// Update an obstacle's movement
+/// </summary>
+/// <param name="obstacle">Obstacle to update</param>
+/// <param name="game">Game of obstacle</param>
+/// <param name="gameScreen">Screen to update</param>
+void Obstacle_UpdateMovement(Obstacle* obstacle, Game* game);
