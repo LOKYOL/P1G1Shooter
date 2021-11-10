c38b0f84192eda0bf5751368857d5b27be92e3b8
Mathis <mathischaussemy@gmail.com>
2021-11-10 11:10:53 +0100

    j'ai enlevé mon debug

--- P1G1Shooter/P1G1Shooter/P1G1ShooterMain.c
+++ P1G1Shooter/P1G1Shooter/P1G1ShooterMain.c
@@ -6,31 +6,11 @@
 #include <time.h>
 #include "Engine/Game.h"
 
-#include "PlayerStruct.h"
-#include "Obstacle.h"
-#include "Projectile.h"
-#include "EnemyShooter.h"
-#include "Enemy.h"
-#include <conio.h>
 #include <crtdbg.h>
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	printf(
-		"Entity : %d\n"
-		"Player : %d\n"
-		"Obstacle : %d\n"
-		"Projectile : %d\n"
-		"EnemyShooter : %d\n"
-		"Enemy K : %d\n"
-		"DisplayZone : %d",
-		sizeof(Entity), sizeof(Player), sizeof(Obstacle), 
-		sizeof(Projectile), sizeof(EnemyShooter), sizeof(Enemy),
-		sizeof(DisplayZone)
-	);
-	_getch();
 
 	Game	game;
 	InitGame(&game);
