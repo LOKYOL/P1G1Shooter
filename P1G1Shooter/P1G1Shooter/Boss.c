af92df6270f4cced980486cf5b5b79888a2abd79
Tristan Léger <49200515+LegerTristan@users.noreply.github.com>
2021-11-12 09:13:02 +0100

    Revert "Boss updated"
    This reverts commit 9e94f3f52baf98726ded0aee10db71ce4ce4b017.

--- P1G1Shooter/P1G1Shooter/Boss.c
+++ P1G1Shooter/P1G1Shooter/Boss.c
@@ -18,29 +18,22 @@ void Boss_Initialize(Boss** _boss,
 
 	newBoss->mEntity.mDisplayZone = _gameScreen->mSprites[TYPE_BOSS];
 
-	newBoss->mEntity.mDisplayZone.mPosY = (WINDOW_HEIGHT - newBoss->mEntity.mDisplayZone.mSizeY) / 2;
+	newBoss->mEntity.mDisplayZone.mPosY = WINDOW_HEIGHT - (newBoss->mEntity.mDisplayZone.mSizeY / 2);
 		//rand() % (WINDOW_HEIGHT - newBoss->mEntity.mDisplayZone.mSizeY);
 
 	newBoss->mEntity.mEntityType = TYPE_BOSS;
 	newBoss->mEntity.mPosition_x = WINDOW_WIDTH;
 	newBoss->mEntity.mPosition_y = newBoss->mEntity.mDisplayZone.mPosY;
 	newBoss->mShootCooldown = 2;
-	newBoss->mContactAttackCooldown = 5;
 	newBoss->mChangeDirectionCooldown = 1;
-	newBoss->mTargetedPosX = 0;
-	newBoss->mTargetedPosY = 0;
 	newBoss->mCurrentDirectionX = (rand() % 3) - 1;
 	newBoss->mCurrentDirectionY = (rand() % 3) - 1;
-	newBoss->mContactAttackStatus = NOT_LAUNCHED;
 }
 
 void Boss_Update(void* _boss, Game* _game, GameScreenData* _gameScreen)
 {
 	Boss* myBoss = (Boss*)_boss;
-
 	Boss_UpdateMovement(myBoss, _gameScreen, _game);
-
-	Boss_ContactAttack(_boss, _gameScreen);
 
 	if (myBoss->mShootCooldown > 0)
 	{
@@ -48,7 +41,7 @@ void Boss_Update(void* _boss, Game* _game, GameScreenData* _gameScreen)
 	}
 	else
 	{
-		Boss_Shoot(myBoss, _gameScreen);
+		Enemy_Shoot(myBoss, _gameScreen);
 	}
 
 	FlushDisplayZone(_game->mDisplaySettings, &myBoss->mEntity.mDisplayZone);
@@ -60,51 +53,39 @@ void Boss_UpdateMovement(Boss* _boss, GameScreenData* _gameScreen, Game* _game)
 		move_x = 0,
 		move_y = 0;
 
-	if (_boss->mContactAttackStatus == ATTACKING)					// Go to targeted position
+	int
+		posPlayer_x = _gameScreen->mPlayer->mEntity.mPosition_x +
+		(_gameScreen->mPlayer->mEntity.mDisplayZone.mSizeX / 2) + 0.5,
+		posPlayer_y = _gameScreen->mPlayer->mEntity.mPosition_y +
+		(_gameScreen->mPlayer->mEntity.mDisplayZone.mSizeY / 2) + 0.5,
+		posEnemy_x = _boss->mEntity.mPosition_x +
+		(_boss->mEntity.mDisplayZone.mSizeX / 2) + 0.5,
+		posEnemy_y = _boss->mEntity.mPosition_y +
+		(_boss->mEntity.mDisplayZone.mSizeY / 2) + 0.5;
+
+	if (_boss->mChangeDirectionCooldown > 0)
 	{
-		if (_boss->mEntity.mPosition_x > _boss->mTargetedPosX)
-		{
-			move_x--;
-		}
-		else
-		{
-			move_x++;
-		}
-		if (_boss->mEntity.mPosition_y > _boss->mTargetedPosY)
-		{
-			move_y--;
-		}
-		else
-		{
-			move_y++;
-		}
+		_boss->mChangeDirectionCooldown -= _game->mGameDt;
 	}
-	else if (_boss->mContactAttackStatus == RETURNING_FROM_ATTACK)	// Return to spawn position
+	else
 	{
-		if (_boss->mEntity.mPosition_x < WINDOW_WIDTH - _boss->mEntity.mDisplayZone.mSizeX)
-		{
-			move_x++;
-		}
-		if (_boss->mEntity.mPosition_y > (WINDOW_HEIGHT - _boss->mEntity.mDisplayZone.mSizeY) / 2)
-		{
-			move_y--;
-		}
-		else
-		{
-			move_y++;
-		}
+		_boss->mChangeDirectionCooldown = 1;
+
+		_boss->mCurrentDirectionX = (rand() % 3) - 1;
+		_boss->mCurrentDirectionY = (rand() % 3) - 1;
 	}
-	else	// Regular pattern to define
+
+	move_y = _boss->mCurrentDirectionY;
+	move_x = _boss->mCurrentDirectionX;
+
+	// Not shooting until moved enough to the left
+	if (posEnemy_x < WINDOW_WIDTH / 2)
 	{
-		int
-			posPlayer_x = _gameScreen->mPlayer->mEntity.mPosition_x +
-			(_gameScreen->mPlayer->mEntity.mDisplayZone.mSizeX / 2) + 0.5,
-			posPlayer_y = _gameScreen->mPlayer->mEntity.mPosition_y +
-			(_gameScreen->mPlayer->mEntity.mDisplayZone.mSizeY / 2) + 0.5,
-			posEnemy_x = _boss->mEntity.mPosition_x +
-			(_boss->mEntity.mDisplayZone.mSizeX / 2) + 0.5,
-			posEnemy_y = _boss->mEntity.mPosition_y +
-			(_boss->mEntity.mDisplayZone.mSizeY / 2) + 0.5;
+		move_x += 2;
+	}
+	else if (posEnemy_x > WINDOW_WIDTH / 5 * 4)
+	{
+		move_x -= 4;
 	}
 
 	// Clamp movement
@@ -120,60 +101,11 @@ void Boss_UpdateMovement(Boss* _boss, GameScreenData* _gameScreen, Game* _game)
 void Boss_Shoot(Boss* _boss, GameScreenData* _gameScreen)
 {
 	Projectile* newProjectile;
-	Proj_Initialize(&newProjectile, 3, 0,
+	Proj_Initialize(&newProjectile, 2, 0,
 		_boss->mEntity.mPosition_x, _boss->mEntity.mPosition_y,
 		TYPE_ENEMY_PROJECTILE, _gameScreen);
 
 	DVectorPushBack(_gameScreen->mAllEntities, &newProjectile);
 
 	_boss->mShootCooldown = 5;
-}
-
-// Launch attack to targeted position (current player position)
-void Boss_LaunchContactAttack(Boss* _boss, GameScreenData* _gameScreen)
-{
-	_boss->mContactAttackStatus = ATTACKING;
-	_boss->mTargetedPosX = _gameScreen->mPlayer->mEntity.mPosition_x;
-	_boss->mTargetedPosY = _gameScreen->mPlayer->mEntity.mPosition_y;
-}
-
-void Boss_ContactAttack(Boss* _boss, GameScreenData* _gameScreen, Game* _game)
-{
-	Entity* targetedPos;
-	targetedPos->mPosition_x = _boss->mTargetedPosX;
-	targetedPos->mPosition_y = _boss->mTargetedPosY;
-
-	if (_boss->mContactAttackCooldown > 0)
-	{
-		_boss->mContactAttackCooldown -= _game->mGameDt;
-	}
-	else if (_boss->mContactAttackCooldown != -1)
-	{
-		Boss_LaunchContactAttack(_boss, _gameScreen);
-	}
-
-	if (_boss->mContactAttackStatus == ATTACKING)
-	{
-		if (Entity_GetDistance(_boss, targetedPos) < 1)
-		{
-			if (Entity_GetDistance(_boss, _gameScreen->mPlayer) < 1)
-			{
-				Entity_TakeDamages(_gameScreen->mPlayer, 1);
-			}
-			_boss->mContactAttackStatus == RETURNING_FROM_ATTACK;
-			_boss->mTargetedPosX = WINDOW_WIDTH - _boss->mEntity.mDisplayZone.mSizeX;
-			_boss->mTargetedPosY = (WINDOW_HEIGHT - _boss->mEntity.mDisplayZone.mSizeY) / 2;
-			_boss->mContactAttackCooldown = -1;
-		}
-	}
-	if (_boss->mContactAttackStatus == RETURNING_FROM_ATTACK)
-	{
-		if (Entity_GetDistance(_boss, targetedPos) < 1)
-		{
-			_boss->mContactAttackStatus == NOT_LAUNCHED;
-			_boss->mTargetedPosX = 0;
-			_boss->mTargetedPosY = 0;
-			_boss->mContactAttackCooldown = 5;
-		}
-	}
 }
